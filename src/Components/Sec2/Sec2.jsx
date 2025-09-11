6033d573826aa28a0dd6100bbd3bf3fabffc3695
Agustin Lovera <agusslovera04@gmail.com>
2025-09-11 11:10:44 -0300

    Include and Finished Sec2

--- src/Components/Sec2/Sec2.jsx
+++ src/Components/Sec2/Sec2.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-
+import graphElementSc2 from "../../assets/elemento_sc2.png";
+import './Sec2_Style.css'
 const Sec2 = () => {
   return (
-    <div>
+    <div className="d-flex flex-column-reverse flex-md-row justify-content-center align-items-center">
+      <div className="col-10 col-md-5">
+        <img src={graphElementSc2} alt="" className="graphElementSc2" />
+      </div>
+      <div className="col-10 col-md-5">
+        <p className="var-txt text-center text-md-left">
+          Hace 3 años que recorro este increíble oficio de la programación.
+          Trabajé y asesoré a distancia y de manera presencial, para agencias,
+          startups, particulares, etc. <br />
+          Me gusta ir más allá del código, integrarme al proyecto, generar un
+          sentido de pertenencia, y construir soluciones que funcionan.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sec2
+export default Sec2;
