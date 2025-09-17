38289f198176f59deec8fab979534046cc49c068
Agustin Lovera <agusslovera04@gmail.com>
2025-09-16 21:03:51 -0300

    Finished

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/AgustinLovera.dev'
+  base: "https://agustin-lovera04.github.io/AgustinLovera.dev/"
 })
